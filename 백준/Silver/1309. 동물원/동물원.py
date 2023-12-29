711b8767850a96d48a673b97b5c30588bf5f2ecc
윤동훈 DongHun Yoon <acttoze1@gmail.com>
2023-12-29 14:35:07 +0900

    [Silver I] Title: 동물원, Time: 160 ms, Memory: 50332 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.py
@@ -0,0 +1,10 @@
+N = int(input())
+dp = [[0] * 3 for _ in range(N)]
+dp[0] = [1,1,1]
+
+for i in range(1,N):
+    dp[i][0] = sum(dp[i-1]) % 9901
+    dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % 9901
+    dp[i][2] = (dp[i-1][0] + dp[i-1][1]) % 9901
+
+print(sum(dp[N-1]) % 9901)
