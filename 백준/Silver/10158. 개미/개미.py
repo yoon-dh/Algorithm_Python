4a13f4bdfa778b0937dc3aa840811ce09a3ec209
윤동훈 DongHun Yoon <acttoze1@gmail.com>
2024-01-10 10:04:19 +0900

    [Silver III] Title: 개미, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/10158. 개미/개미.py
@@ -0,0 +1,13 @@
+w, h = map(int, input().split())
+p, q = map(int, input().split())
+t = int(input())
+
+a, b = (p + t) // w, (q + t) // h     
+
+if a % 2 == 0: x = (p + t) % w
+else: x = w - (p + t) % w
+
+if b % 2 == 0: y = (q + t) % h
+else: y = h - (q + t) % h
+
+print(x, y)
