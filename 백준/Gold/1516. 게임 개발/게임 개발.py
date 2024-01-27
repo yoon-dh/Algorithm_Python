4f82a04a0c629756268f297232aeddc251300082
윤동훈 DongHun Yoon <acttoze1@gmail.com>
2024-01-27 15:14:34 +0900

    [Gold III] Title: 게임 개발, Time: 88 ms, Memory: 34068 KB -BaekjoonHub

---
+++ 백준/Gold/1516. 게임 개발/게임 개발.py
@@ -0,0 +1,38 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+def T_sort():
+    Q = deque()
+    dp = [0] * (N+1)
+    for i in range(1,N+1):
+        if line_cnt[i] == 0:
+            Q.append(i)
+            dp[i] = time[i]
+
+    while Q:
+        current_node = Q.popleft()
+        for i in G[current_node]:
+            line_cnt[i] -= 1
+            dp[i] = max(dp[i],dp[current_node] + time[i])
+            if line_cnt[i] == 0:
+                Q.append(i)
+    return dp[1:]
+
+# =======================================
+
+N = int(input())
+G = [[] for _ in range(N+1)]
+line_cnt = [0 for _ in range(N+1)]
+time = [0 for _ in range(N+1)]
+
+for i in range(1,N+1):
+    tmp = list(map(int, input().split()))[:-1]
+    time[i] = tmp[0]
+    for j in tmp[1:]:
+        G[j].append(i)
+        line_cnt[i] += 1
+
+ans = T_sort()
+for i in ans:
+    print(i)
