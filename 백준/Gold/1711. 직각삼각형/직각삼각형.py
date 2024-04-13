a33ec6a72c96c498c57edf8af35d92ab14407690
윤동훈 DongHun Yoon <acttoze1@gmail.com>
2024-04-13 21:20:56 +0900

    [Gold V] Title: 직각삼각형, Time: 13316 ms, Memory: 111580 KB -BaekjoonHub

---
+++ 백준/Gold/1711. 직각삼각형/직각삼각형.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = [tuple(map(int, input().split())) for _ in range(N)]
+ans = 0
+
+for i in range(N-2):
+    for j in range(i+1, N-1):
+        for k in range(j+1, N):
+            dot1, dot2, dot3 = arr[i], arr[j], arr[k]
+            d1 = (dot1[0]-dot2[0])**2 + (dot1[1]-dot2[1])**2
+            d2 = (dot2[0]-dot3[0])**2 + (dot2[1]-dot3[1])**2
+            d3 = (dot3[0]-dot1[0])**2 + (dot3[1]-dot1[1])**2
+            
+            if 2*max(d1,d2,d3) == d1+d2+d3:
+                ans += 1
+print(ans)
